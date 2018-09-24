53144b3e2e22a1b122336e2da736bf1890e56f76
Solidtony Desktop <anthonygarcia918@hotmail.com>
2018-09-24 17:04:09 -0600

    Initial commit for GitHub.

---
+++ public/js/script.js
@@ -0,0 +1,28 @@
+    //loads clustered data from ./data/ClusterData.csv
+    d3.csv("data/Literature_all.csv", function(error, starData)
+    {
+        let GPlot = new Galaxy(starData);
+        GPlot.create();
+
+        //loads ptable data from ./data/ptable.csv
+        d3.csv("data/ptable.csv", function (error, elements)
+        {
+            let pTable = new PTable(elements, GPlot, starData);
+            pTable.create();
+        });
+
+        //scatter plot
+        let xActive = document.getElementById("xselect").value;
+        let yActive = document.getElementById("yselect").value;
+        let cActive = document.getElementById("cselect").value;
+        this.SPlot = new ScatterPlot(starData);
+        SPlot.update(xActive, yActive, cActive);
+    });
+
+function chooseSelection()
+{
+    let xActive = document.getElementById("xselect").value;
+    let yActive = document.getElementById("yselect").value;
+    let cActive = document.getElementById("cselect").value;
+    this.SPlot.update(xActive, yActive, cActive)
+}
